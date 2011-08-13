90d8adaba5188499fc90a0cb2a5838b64417bb2d
elliotwoods <elliot.woods@gmail.com>
2011-08-13 21:05:41 +0100

    removed platform files just use simple throwError/throwWarning inheritance

--- src/TalkyPlatform.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  TalkyPlatform.cpp
-//  ofxTalkyExampleServer
-//
-//  Created by Elliot Woods on 13/08/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
